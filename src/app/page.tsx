b7be02e2c55ff40cc82a1c2038c315192eccfbe8
mihna123 <mihailonvojinovic@gmail.com>
2026-05-24 21:28:35 +0200

    feat: add receipt emoji to the title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { InvoiceGenerator } from '@/components/invoice-generator';
 export default function Home() {
   return (
     <main className="space-y-4 p-2">
-      <h1 className="text-xl">Free Invoice Generator</h1>
+      <h1 className="text-xl">🧾 Free Invoice Generator</h1>
       <InvoiceGenerator />
     </main>
   );

c6eea92f2a3016463f0c4b8e4b813afd846bf821
mihna123 <mihailonvojinovic@gmail.com>
2026-06-02 00:48:08 +0200

    feat: add footer and contact info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import { Footer } from '@/components/footer';
 import { InvoiceGenerator } from '@/components/invoice-generator';
 
 export default function Home() {
   return (
-    <main className="space-y-4 p-2">
-      <h1 className="text-center text-xl font-bold">
-        🧾 Free Invoice Generator
-      </h1>
-      <section className="flex items-center justify-center">
-        <InvoiceGenerator />
+    <main className="pt-2">
+      <section className="flex h-[100vh] flex-col items-center justify-between">
+        <div className="space-y-10">
+          <h1 className="text-center text-2xl font-bold">
+            🧾 Free Invoice Generator
+          </h1>
+          <InvoiceGenerator />
+        </div>
+        <Footer />
       </section>
     </main>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full bg-gray-400 p-2 text-center">
+      <p className="font-bold text-white">
+        Having issues? Contact me here:{' '}
+        <a
+          href="mailto:mihailonvojinovic@gmail.com"
+          className="font-normal text-blue-700 underline"
+        >
+          mihailonvojinovic@gmail.com
+        </a>
+      </p>
+    </footer>
+  );
+};

c2d33aa0c52bc2bee61b0195c30e37f81cafa643
mihna123 <mihailonvojinovic@gmail.com>
2026-06-03 17:00:50 +0200

    Revert "feat: add google analytics script"
    This reverts commit 728fb96a78b7808a87e52cbc2f4bb0cbfb4f0f74.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import './globals.css';
 
 const geistSans = Geist({
@@ -30,7 +29,6 @@ export default function RootLayout({
       >
         {children}
       </body>
-      <GoogleAnalytics gaId="G-PPNQQX1XCD" />
     </html>
   );
 }

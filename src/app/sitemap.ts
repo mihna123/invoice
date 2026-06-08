edb268a115220d329f912c507ceeb13c2ca5ab8a
mihna123 <mihailonvojinovic@gmail.com>
2026-06-09 00:17:55 +0200

    feat: add a sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+import { getAllBlogs } from '@/lib/blogs/api';
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const blogs = getAllBlogs();
+  const blogUrls = blogs.map((blog) => ({
+    url: `https://free-incoices.org/blogs/${blog.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'yearly' as const,
+    priority: 0.8,
+  }));
+
+  return [
+    {
+      url: 'https://free-invoices.org',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://free-invoices.org/help',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://free-invoices.org/blogs',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    ...blogUrls,
+  ];
+}
